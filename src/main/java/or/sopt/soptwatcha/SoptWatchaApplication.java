d36cfb76b14c8174739c8a423ce5a85f6b7dbd49
gdbs1107 <gdbs1107@naver.com>
2025-05-08 22:02:09 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/or/sopt/soptwatcha/SoptWatchaApplication.java
@@ -0,0 +1,13 @@
+package or.sopt.soptwatcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoptWatchaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoptWatchaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/or/sopt/soptwatcha/SoptWatchaApplicationTests.java
@@ -0,0 +1,13 @@
+package or.sopt.soptwatcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoptWatchaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
