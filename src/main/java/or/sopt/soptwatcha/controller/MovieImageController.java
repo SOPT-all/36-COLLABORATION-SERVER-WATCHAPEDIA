a14eb857bc00167d44c57cf491c7026afaff1481
gdbs1107 <gdbs1107@naver.com>
2025-05-12 15:57:41 +0900

    feat: 스웨거 명세 추가

--- src/main/java/or/sopt/soptwatcha/controller/MovieImageController.java
+++ src/main/java/or/sopt/soptwatcha/controller/MovieImageController.java
@@ -1,5 +1,7 @@
 package or.sopt.soptwatcha.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import or.sopt.soptwatcha.domain.MovieImage;
 import or.sopt.soptwatcha.service.MovieImageServiceImpl;
@@ -10,10 +12,12 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/movie-image")
 @RequiredArgsConstructor
+@Tag(name = "영화 포스터 관련 API")
 public class MovieImageController {
 
     private final MovieImageServiceImpl movieImageService;
 
+    @Operation(summary = "영화 포스터 업로드 API")
     @PostMapping(path = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String save(@RequestParam("file") MultipartFile file,
                        @RequestParam("movieId") Long movieId) {
@@ -23,6 +27,8 @@ public class MovieImageController {
 
 
     @GetMapping
+    @Operation(summary = "영화 포스터 조회 API",
+    description = "현재는 filename으로 조회하고 있지만, 추후 영화/시리즈 객체를 이용하여 한 번에 조회 할 수 있도록 업데이트 될 예정입니다")
     public String getImage(String fileName){
         return movieImageService.findByFileName(fileName);
     }
