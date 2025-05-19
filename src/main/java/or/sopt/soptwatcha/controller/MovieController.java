199456bad6507ae698a942e540d91254f006c3a9
gdbs1107 <gdbs1107@naver.com>
2025-05-19 15:39:59 +0900

    feat: API 명세 추가

--- src/main/java/or/sopt/soptwatcha/controller/MovieController.java
+++ src/main/java/or/sopt/soptwatcha/controller/MovieController.java
@@ -31,7 +31,8 @@ public class MovieController {
 
 
     @GetMapping("posts/ranking")
-    @Operation(summary = "내가 좋아할 만한 작품 랭킹 API")
+    @Operation(summary = "내가 좋아할 만한 작품 랭킹 API",
+    description = "작품에 따른 예상 별점으로 다섯개의 작품 리스트를 반환합니다")
     public BaseResponse<GetMovieTopRankingResponseDTO.GetMovieTopRankingResponseListDTO> getRankingMovies() {
 
         GetMovieTopRankingResponseDTO.GetMovieTopRankingResponseListDTO result = movieService.getMovieTopRanking();
@@ -41,7 +42,8 @@ public class MovieController {
 
 
     @GetMapping("posts/soon")
-    @Operation(summary = "개봉 예정작 조회 API")
+    @Operation(summary = "개봉 예정작 조회 API",
+    description = "개봉예정일과 가장 가까운 작품 다섯개를 리스트로 반환합니다 <br><br> **movieType**에는 영화라면 MOVIE,시리즈라면 SERIES 를 넣어주세요")
     public BaseResponse<GetMovieSoonResponseDTO.GetMovieSoonResponseListDTO> getSoonMovies(@RequestParam String movieType) {
 
         GetMovieSoonResponseDTO.GetMovieSoonResponseListDTO result = movieService.getSoon(movieType);
