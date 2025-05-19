93552282acccf0ba71d6d5692e84fe431b331fc3
gdbs1107 <gdbs1107@naver.com>
2025-05-19 15:27:20 +0900

    feat: tranaction 메서드단에 구현'

--- src/main/java/or/sopt/soptwatcha/service/MovieService.java
+++ src/main/java/or/sopt/soptwatcha/service/MovieService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface MovieService {
+
+    @Transactional(readOnly = true)
     GetPreferenceMoviesListResponse getPreferenceMovies(Long commentId);
 
     @Transactional(readOnly = true)
