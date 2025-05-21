8f07466bdee08934c23c57458ec6459aa67ded81
gdbs1107 <gdbs1107@naver.com>
2025-05-21 11:15:01 +0900

    feat: base url 추가

--- src/main/java/or/sopt/soptwatcha/config/SwaggerConfig.java
+++ src/main/java/or/sopt/soptwatcha/config/SwaggerConfig.java
@@ -31,7 +31,7 @@ public class SwaggerConfig {
                         .bearerFormat("JWT"));
 
         return new OpenAPI()
-                .addServersItem(new Server().url("/"))
+                .addServersItem(new Server().url("/www.soptcha.shop"))
                 .info(info)
                 .addSecurityItem(securityRequirement)
                 .components(components);
